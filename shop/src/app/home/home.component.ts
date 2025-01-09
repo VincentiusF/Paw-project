49276a6a1e3f9a9f5a080838ff1c55b37bbdd7d4
dimas1509 <mdimasdimas0915@gmail.com>
2025-01-09 17:59:13 +0700

    account

--- shop/src/app/home/home.component.ts
+++ shop/src/app/home/home.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  userName: string = 'Dimas'; // Ganti dengan nama pengguna yang diambil dari sistem autentikasi
 
+  logout(): void {
+    // Tambahkan logika logout di sini
+    console.log('User logged out');
+    window.location.href = '/login'; // Arahkan ke halaman login
+  }
 }
