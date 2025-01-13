85239d790fbd11e2b3d7f4573cba30e86cc8f4cf
VincentiusFernando <vincentiusfernando_2226240032@mhs.mdp.ac.id>
2025-01-13 17:52:15 +0700

    vercel

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -9,7 +9,7 @@ import { environment } from '../../environments/environment';
   providedIn: 'root',
 })
 export class UserService {
-  private url: string = 'http://localhost:3000/users/';
+  private url: string = 'mongodb://mdp:BelajarMongo2024@cluster0-shard-00-00.n214x.mongodb.net:27017,cluster0-shard-00-01.n214x.mongodb.net:27017,cluster0-shard-00-02.n214x.mongodb.net:27017/dbbuku?ssl=true&replicaSet=atlas-107w2z-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0';
   // private url: string = environment.api + 'users/';
   private subjectExecuteUser = new Subject<string>();
 
